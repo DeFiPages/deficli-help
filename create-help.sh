14b4f60b0b02536b47ece2a7220c9ae6b99c806b
DeFiPages <defichainpages@gmail.com>
2023-06-11 08:23:35 +0000

    use changi parameter

--- create-help.sh
+++ create-help.sh
@@ -2,9 +2,19 @@
 #set -x #Print a trace of simple commands (echo on)
 #set -e #Exit immediately if error
 
+#define defi-cli for changi
+home=$(eval echo ~$USER)
+
+if [[ -d "$home/.defi/changi/" ]]
+then
+  defi_cli=("$home/.defi/defi-cli" "-changi")
+else
+  defi_cli=("defi-cli")
+fi
+
 toc()
 {
-defi-cli help | while read line ; do
+"${defi_cli[@]}" help | while read line ; do
   [ -z "$line" ] && continue  # ignore empty line
   arr=($line)
   firstword=${arr[0]}
@@ -18,12 +28,12 @@ done
 
 help()
 {
-defi-cli --version
+"${defi_cli[@]}" --version
 echo
 toc
 echo
 
-defi-cli help| while read line ; do
+"${defi_cli[@]}" help| while read line ; do
   [ -z "$line" ] && continue  # ignore empty line
   arr=($line)
   firstword=${arr[0]}
@@ -34,7 +44,7 @@ defi-cli help| while read line ; do
   else
     echo "<details><summary>$line</summary><p>"
     #append two spaces for markdown linebreak at end of every line.
-    defi-cli help $firstword | sed ':a;N;$!ba;s/\n/  \n/g'
+    "${defi_cli[@]}" help $firstword | sed ':a;N;$!ba;s/\n/  \n/g'
     echo "</p></details>"
     echo
   fi
